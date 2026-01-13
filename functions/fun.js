6b53fdaf94b9bdcc09f87ace38b39c23843d74a6
Shreekant910 <hajareshreekant@gmail.com>
2026-01-13 21:49:22 +0530

    practice

--- functions/fun.js
+++ functions/fun.js
@@ -1,38 +1,73 @@
-function makeTea(typeofTea){
-     return `Making ${typeofTea}`
-}
+// function makeTea(typeofTea){
+//      return `Making ${typeofTea}`
+// }
 
-let teaOrder = makeTea("green tea")
-
-
-function orderTea(teaType){
-    function confirmOrder(){
-        return `Order confirmed for chai`;
-    }
-    return confirmOrder();
-}
-
-let orderConfirmation = orderTea("chai");
-
-const calulateTotal =(price,quantity)=>{
-
-    return price*quantity;
-}
-let totalCost = calulateTotal(5,10);
-// console.log(totalCost);
+// let teaOrder = makeTea("green tea")
 
 
-function processTeaOrder(teaFunction){
-   return teaFunction("earl gray");
-}
-let finalOrder = processTeaOrder(makeTea);
-// console.log(finalOrder);
+// function orderTea(teaType){
+//     function confirmOrder(){
+//         return `Order confirmed for chai`;
+//     }
+//     return confirmOrder();
+// }
 
-function createTeaMaker(){
+// let orderConfirmation = orderTea("chai");
+
+// const calulateTotal =(price,quantity)=>{
+
+//     return price*quantity;
+// }
+// let totalCost = calulateTotal(5,10);
+// // console.log(totalCost);
+
+
+// function processTeaOrder(teaFunction){
+//    return teaFunction("earl gray");
+// }
+// let finalOrder = processTeaOrder(makeTea);
+// // console.log(finalOrder);
+
+// function createTeaMaker(){
     
-    return function (teaType){
-        return `making ${teaType}`
+//     return function (teaType){
+//         return `making ${teaType}`
+//     }
+// }
+// let teaMaker = createTeaMaker();
+// console.log(teaMaker());
+
+// function nonrepeate(str){
+//     const obj={};
+//     for(let char of str){
+//         obj[char]=(obj[char]||0)+1;
+        
+//     }
+    
+//    console.log()
+//     for (let key in obj) {
+//         if (obj[key] === 1) {
+//             console.log(key);
+//         }
+//     }
+// }
+// console.log(nonrepeate("ssrrhheekkaattz")); 
+
+
+
+function firstnonrepeate(str){
+    const obj={};
+    for(let char of str){
+        obj[char]=(obj[char]||0)+1;
+        
+    }
+    
+   console.log()
+    for (let key in obj) {
+        if (obj[key] === 1) {
+            console.log(key);
+            return;
+        }
     }
 }
-let teaMaker = createTeaMaker();
-console.log(teaMaker());
+console.log(firstnonrepeate("fssrrhheekkaattz")); 
