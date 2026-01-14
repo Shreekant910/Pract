9ab15e460359c9e77959fbc9c078b9bfbb7d48a3
Shreekant910 <hajareshreekant@gmail.com>
2026-01-14 19:24:53 +0530

    practice

--- functions/fun.js
+++ functions/fun.js
@@ -55,19 +55,83 @@
 
 
 
-function firstnonrepeate(str){
-    const obj={};
-    for(let char of str){
-        obj[char]=(obj[char]||0)+1;
+// function firstnonrepeate(str){
+//     const obj={};
+//     for(let char of str){
+//         obj[char]=(obj[char]||0)+1;
         
-    }
+//     }
     
-   console.log()
-    for (let key in obj) {
-        if (obj[key] === 1) {
-            console.log(key);
-            return;
-        }
-    }
+//    console.log()
+//     for (let key in obj) {
+//         if (obj[key] === 1) {
+//             console.log(key);
+//             return;
+//         }
+//     }
+// }
+// console.log(firstnonrepeate("fssrrhheekkaattz")); 
+
+
+// function checkPalindrome(string){
+//     const reversed = string.split('').reverse().join('');
+//     console.log(string.split('').reverse().join(''))
+//     if(reversed === string){
+//         return true;
+//     }
+//     return false;
+// }
+
+// console.log(checkPalindrome("mamd"));
+
+
+
+// const arr = [1, 2, 2, 3, 4, 4] ;
+
+// function removeDupli(arr){
+//     const obj={}
+//     for(let num of arr){
+//         obj[num]=(obj[num]||0)+1;
+//     }
+//     const uniqueArr = [];
+//     for(let key in obj){
+//         uniqueArr.push(Number(key));
+//     }
+//     return uniqueArr;
+// }
+// console.log(removeDupli(arr));
+
+// function largestNum(arr){
+//     console.log(arr)
+//     let largest;
+//     for(i=0;i<arr.length;i++){
+//         if(arr[i]<arr[i+1]){
+//             largest = arr[i+1];
+//         }
+//     }
+//     return largest;
+// }
+// const newarr=[2,3,4,5,6,7,8,9]
+// console.log(largestNum(newarr));
+
+
+// function reverseString(newString){
+//     const arr=[];
+//     const reverse = newString.split('');
+//     console.log(reverse)
+//     for(i=newString.length;i>=0;i--){
+//         arr.push(reverse[i]);
+//     }
+//     return arr.join('');
+// }
+
+// console.log(reverseString("abcde"));
+
+function sayHi() {
+  console.log(name);
+//   console.log(age);
+  var name = 'Lydia';
+  let age = 21;
 }
-console.log(firstnonrepeate("fssrrhheekkaattz")); 
+
+sayHi();
